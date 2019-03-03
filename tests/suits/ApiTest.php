4b6873aac4275744e001736f65f12cdfdbbfc273
Marcin Pudełek <mrcnpdlk@gmail.com>
2019-03-03 17:17:11 +0100

    tests

--- tests/suits/ApiTest.php
+++ tests/suits/ApiTest.php
@@ -7,8 +7,9 @@
 
 namespace Tests\Tvsat\Api\Xmdb;
 
-
 use Mrcnpdlk\Api\Unoconv\Api;
+use Mrcnpdlk\Api\Unoconv\Config;
+use Mrcnpdlk\Api\Unoconv\Exception\UnoconvException;
 use PHPUnit\Framework\TestCase;
 
 class ApiTest extends TestCase
@@ -20,5 +21,18 @@ class ApiTest extends TestCase
     {
         $oApi = new Api();
         $this->assertTrue(true);
+    }
+
+    /**
+     * @throws \Mrcnpdlk\Api\Unoconv\Exception
+     */
+    public function testConstruct_invalid(): void
+    {
+        $this->expectException(UnoconvException::class);
+        $config = new Config([
+            'binary' => '/some/binary/file',
+        ]);
+        $oApi   = new Api();
+        $oApi->create('sss.docx', 'sss.pdf');
     }
 }
