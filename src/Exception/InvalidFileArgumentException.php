f7e978478d248f2112bfd057bf8c3d16aef203a4
Marcin Pudełek <mrcnpdlk@gmail.com>
2019-03-03 18:59:33 +0100

    default extension

---
+++ src/Exception/InvalidFileArgumentException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by Marcin.
+ * Date: 03.03.2019
+ * Time: 18:27
+ */
+
+namespace Mrcnpdlk\Api\Unoconv\Exception;
+
+use Mrcnpdlk\Api\Unoconv\Exception;
+
+/**
+ * Class InvalidFileArgumentException
+ */
+class InvalidFileArgumentException extends Exception
+{
+}
