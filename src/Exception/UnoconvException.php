ee9685cf340ad25eaac05cbf02de2735b467b16d
Marcin Pudełek <mrcnpdlk@gmail.com>
2019-03-03 17:16:53 +0100

    UnoconvException

---
+++ src/Exception/UnoconvException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Marcin.
+ * Date: 03.03.2019
+ * Time: 17:11
+ */
+
+namespace Mrcnpdlk\Api\Unoconv\Exception;
+
+
+use Mrcnpdlk\Api\Unoconv\Exception;
+
+class UnoconvException extends Exception
+{
+
+}
