f58bb3c014a1dbadee75ace835ed128a455feb8e
Marcin Pudełek <mrcnpdlk@gmail.com>
2019-03-11 18:15:38 +0100

    tests

--- tests/suits/ApiTest.php
+++ tests/suits/ApiTest.php
@@ -10,9 +10,11 @@ namespace Tests\Mrcnpdlk\Api\Unoconv;
 use Mrcnpdlk\Api\Unoconv\Api;
 use Mrcnpdlk\Api\Unoconv\Config;
 use Mrcnpdlk\Api\Unoconv\Enum\FormatType;
+use Mrcnpdlk\Api\Unoconv\Exception\DomainException;
 use Mrcnpdlk\Api\Unoconv\Exception\InvalidFileArgumentException;
 use Mrcnpdlk\Api\Unoconv\Exception\UnoconvException;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\NullLogger;
 
 class ApiTest extends TestCase
 {
@@ -53,10 +55,53 @@ class ApiTest extends TestCase
     public function testConstruct_invalid_2(): void
     {
         $this->expectException(InvalidFileArgumentException::class);
-        $config   = new Config([
+        $config = new Config([
             'binary' => '/some/binary/file',
         ]);
-        $oApi     = new Api();
+        $oApi   = new Api();
         $oApi->transcode('foo_bar.test', FormatType::PDF(), null);
+    }
+
+    /**
+     * @throws \Mrcnpdlk\Api\Unoconv\Exception
+     * @throws \Mrcnpdlk\Api\Unoconv\Exception\ConfigurationException
+     * @throws \Mrcnpdlk\Api\Unoconv\Exception\DomainException
+     * @throws \Mrcnpdlk\Api\Unoconv\Exception\InvalidFileArgumentException
+     * @throws \Mrcnpdlk\Api\Unoconv\Exception\UnoconvException
+     */
+    public function testConstruct_invalid_3(): void
+    {
+        $this->expectException(DomainException::class);
+        $config   = new Config([
+            'binary' => '/some/binary/file',
+            'logger' => new NullLogger(),
+        ]);
+        $oApi     = new Api($config);
+        $testFile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'test.docx';
+        file_put_contents($testFile, null);
+        $oApi->transcode($testFile, FormatType::PDF(), sys_get_temp_dir(), [
+            'foo_bar' => null,
+        ]);
+        @unlink($testFile);
+    }
+
+    /**
+     * @throws \Mrcnpdlk\Api\Unoconv\Exception
+     * @throws \Mrcnpdlk\Api\Unoconv\Exception\DomainException
+     * @throws \Mrcnpdlk\Api\Unoconv\Exception\InvalidFileArgumentException
+     * @throws \Mrcnpdlk\Api\Unoconv\Exception\UnoconvException
+     */
+    public function testConstruct_invalid_4(): void
+    {
+        $this->expectException(UnoconvException::class);
+
+        $oApi     = new Api();
+        $testFile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'test.docx';
+        file_put_contents($testFile, null);
+        $oApi->transcode($testFile, FormatType::PDF(), sys_get_temp_dir(), [
+            'foo_string' => 'foo',
+            'foo_bool'   => true,
+        ]);
+        @unlink($testFile);
     }
 }
